3ab2c076a59a73bdb4c1974de3946a9b5f67dc9a
Andrey Begunov <aandreybegunov@yandex.ru>
2020-08-18 12:06:17 +0300

    added error handler

---
+++ middlewares/error-handler.js
@@ -0,0 +1,24 @@
+const HttpError = require('../libs/http-error');
+
+module.exports = (error, req, res, next) => {
+  if (typeof error === 'number') {
+    error = new HttpError(error);
+  }
+
+  if (error instanceof HttpError) {
+    const { status, message } = error;
+    res.sendStatus(status).json({ message });
+  }
+
+  if (Array.isArray(error)) {
+    error = {
+      status: 400,
+      message: error,
+    };
+  }
+
+  res.status(error.status || 500).json({
+    status: error.status,
+    message: error.message,
+  });
+};
