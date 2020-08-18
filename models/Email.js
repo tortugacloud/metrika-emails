f0a8c9052e05eea46c63bb8f77acc3f63db46c18
Andrey Begunov <aandreybegunov@yandex.ru>
2020-08-18 12:52:06 +0300

    added email schemas

---
+++ models/Email.js
@@ -0,0 +1,42 @@
+const { model, Schema } = require('../libs/mongoose');
+
+//  transitions on site
+
+const OpenEmailSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+const UnsubscribeListSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+const TransitionListSchena = new Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+module.exports = {
+  OpenEmail: model('OpenEmail', OpenEmailSchema),
+  UnsubscribeList: model('UnsubscribeList', UnsubscribeListSchema),
+  TransitionList: model('TransitionList', TransitionListSchena),
+};
