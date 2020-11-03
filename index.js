6f1fbd899bc7c17897d7b615237af6c4bc57624d
Andrey Begunov <aandreybegunov@yandex.ru>
2020-11-03 13:48:06 +0200

    added images

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const compression = require('compression');
 const helmet = require('helmet');
+const path = require('path');
 const errorHandler = require('./middlewares/error-handler');
 const { NODE_ENV, PORT } = process.env;
 
@@ -9,6 +10,7 @@ const app = express();
 app.use(compression());
 app.use(helmet());
 app.use(express.json());
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 if (NODE_ENV === 'development') {
   const morgan = require('morgan');
@@ -16,6 +18,8 @@ if (NODE_ENV === 'development') {
 }
 
 require('./routes')(app);
+
+app.get('/', (req, res) => res.send('ok'));
 
 app.all('/*', (req, res, next) => next(404));
 
